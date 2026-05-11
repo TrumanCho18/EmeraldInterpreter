0b0ab8568db0c171ebf5012a6211270339517c9e
Truman Cho <tlc101809@gmail.com>
2026-05-10 23:49:34 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,129 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> lines = getFileData("src/Shell");
+        ArrayList<ArrayList<String>> Memory = new ArrayList<>();
+
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+
+            if (line.split(" ")[0].equals("print")) { //PRINT
+
+                String Query;
+                if (line.contains("'")) {
+                    Query = line.split("'")[1];
+                } else {
+                    Query = line.split(" ")[1];
+                    Query = Query.substring(1, Query.length() - 1);
+                }
+
+                if (line.contains("'")) {
+                    System.out.println(Query);
+                }
+                else {
+                    for (int j = 0; j < Memory.size(); j++) {
+                        if (Memory.get(j).get(0).equals(Query)) {
+                            System.out.println(Memory.get(j).get(2));
+                        }
+                    }
+                }
+            }
+            else { //VARIABLE DECLARATION
+                String varName = line.split(" ")[0];
+                String varType = "null";
+                String val = "null";
+                if (line.contains("'")) {
+                    varType = "String";
+                    val = line.split("'")[1];
+                } else {
+                    val = line.split(" ")[2];
+                }
+
+
+                if (isInt(val)) {
+                    varType = "Integer";
+                    val = line.split(" ")[2];
+                } else if (isDouble(val)) {
+                    varType = "Double";
+                    val = line.split(" ")[2];
+                } else {
+
+                }
+
+                ArrayList<String> Cache = new ArrayList<>();
+                Cache.add(varName);
+                Cache.add(varType);
+                Cache.add(val);
+
+                Memory.add(Cache);
+
+            }
+        }
+        SpitMemory(Memory);
+    }
+
+    public static void SpitMemory(ArrayList<ArrayList<String>> Memory) {
+        System.out.println();
+        System.out.println("--MEMORY CACHES--");
+        for (int i = 0; i < Memory.size(); i++) {
+            for (int j = 0; j < Memory.get(i).size(); j++) {
+                System.out.print(Memory.get(i).get(j) + " | ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean isDouble(String str) {
+        if (str == null) {
+            return false;
+        }
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static boolean isInt(String str) {
+        if (str == null) {
+            return false;
+        }
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static boolean isStr(String str) {
+        if (str == null) {
+            return false;
+        }
+        return str.charAt(0) == '\'' && str.charAt(str.length() - 1) == '\'';
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+}
